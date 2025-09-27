35345b47d47d9469179303bda5901b1297ca01cf
Marcel Debruyker <marcel.debruyker@gmail.com>
2025-09-27 08:23:06 +0200

    fix: revert to serverless vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/edge';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://docs.astro.build/reference/configuration-reference/
 export default defineConfig({
